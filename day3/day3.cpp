6234fa48179fe56935ee16997957e0afc3a1141b
RyanButler53 <rmbutler@outlook.com>
2025-12-06 12:39:27 -0800

    redo day3 part2 as dynamic programming rather than memoization. Also consolidate both into 1 solve function

--- day3/day3.cpp
+++ day3/day3.cpp
@@ -3,13 +3,14 @@
 #include <fstream>
 #include <tuple>
 #include <vector>
-#include <string_view>
+#include <print>
 #include <ranges>
 #include <print>
 #include <map>
 #include <algorithm>
 #include <iomanip>
-#include <optional> // monads!
+#include <mdspan>
+#include "matrix.hpp"
 #include <functional>
 
 std::vector<std::vector<int>> parse(std::filesystem::path file){
@@ -17,7 +18,6 @@ std::vector<std::vector<int>> parse(std::filesystem::path file){
     std::string line;
     std::vector<std::vector<int>> result;
     while(std::getline(in, line)){
-        // result.push_back(line);
         std::vector<int> digits;
         for(char c : line){
             digits.push_back(atoi(&c));
@@ -27,69 +27,49 @@ std::vector<std::vector<int>> parse(std::filesystem::path file){
     return result;
 }
 
-int getJoltage(const std::vector<int>& batteries){
-    std::vector<int>::const_iterator first = std::max_element(batteries.begin(), batteries.end()- 1);
-    std::vector<int>::const_iterator second = std::max_element(first + 1, batteries.end());
- 
-    return *first * 10 + *second; 
+int64_t joltage(const std::vector<int>& batteries, size_t k){
+    size_t n = (batteries.size() + 1);
+    Matrix<int64_t> table(k+1, n);
+    // Base Case:  No more numbers to pick. Always 0
+    for (size_t basecase = 0; basecase < n; ++basecase){
+        // table(basecase, 0) = 0;
+        table(0, basecase) = 0;
+    }
+
+    for (size_t icol = 0; icol < batteries.size(); ++icol){
+        size_t col = batteries.size() - icol - 1;
+        for (size_t ik = 1; ik < k+1; ++ik){
+            // Recursive Logic: 
+            if (ik + col == batteries.size()){
+                // Must use all batteries. 
+                table(ik, col) = (batteries[col] * pow(10, ik-1)) + table(ik - 1, col + 1);
+                break;
+            } else if (batteries[col] < batteries[col+1]){
+                // Skip the battry and use the next one
+                table(ik, col) = table(ik, col + 1);
+            } else {
+                int64_t useit = (batteries[col] * pow(10, ik-1)) + table(ik - 1, col + 1);
+                int64_t loseit = table(ik, col + 1);
+                table(ik, col) = std::max(useit, loseit);
+            }
+        }
+    }
+
+    return table(k,0);
 }
 
-
-uint64_t part1(std::filesystem::path p){
-    auto input = parse(p);
+int64_t solve(std::filesystem::path file, size_t n){
+    auto input = parse(file);
     uint64_t sum = 0;
     for (const auto& battery : input){
-        sum += getJoltage(battery);
+        sum += joltage(battery, n);
     }
     return sum;
 }
-
-// Memoize:
-int64_t joltageHelper(const std::vector<int>& batteries, size_t i, size_t n, std::map<std::pair<size_t, size_t>, int64_t>& memo){
-    // Use memo if possible
-    if (memo.contains({i, n})){
-        return memo.at({i, n});
-    } else if (n == 0){
-        // Base Case: No more numbers left -> no more joltage to add
-        memo.insert({{i, 0}, 0});
-        return 0;
-    } else if (i + n == batteries.size()){
-        // Must use all the remaining batteries 
-        int64_t result = (batteries[i] * pow(10, n-1)) + joltageHelper(batteries, i+1, n-1, memo);
-        memo.insert({{i, n}, result});
-        return result;
-    } else if (batteries[i] < batteries[i+1]){
-        // Branch and bound: Its always better to use the next battery 
-        int64_t result = joltageHelper(batteries, i+1, n,memo);
-        memo.insert({{i, n}, result});
-        return result;
-    } else {
-        int64_t useit = (batteries[i] * pow(10, n-1)) + joltageHelper(batteries, i+1, n-1,memo);
-        int64_t loseit = joltageHelper(batteries, i+1, n,memo);
-        int64_t result = std::max(useit, loseit);
-        memo.insert({{i, n}, result});
-        return result;
-    }
-}
-
-int64_t getJoltage2(const std::vector<int>& batteries){
-    std::map<std::pair<size_t, size_t>, int64_t> memo;
-    return joltageHelper(batteries, 0, 12, memo);    
-}
-
-
-uint64_t part2(std::filesystem::path p){
-    auto input = parse(p);
-    uint64_t sum = 0;
-    for (const auto& battery : input){
-        sum += getJoltage2(battery);
-    }
-    return sum;
-}
-
 
 int main(int argc, char** argv){
 
-    std::cout << part1(argv[1]) << std::endl; 
-    std::cout << part2(argv[1]) << std::endl;
+    std::cout << solve(argv[1], 2) << std::endl; 
+    std::cout << solve(argv[1], 12) << std::endl;
+
 }
